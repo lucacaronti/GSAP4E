eb8cbc5af8c3ddad5dce76c7c7c25694e81c41a5
lucacaronti <carontiluca@gmail.com>
2024-07-27 18:54:32 +0200

    WIP #2 Improved ReadMe and animations

--- GSAP4E.js
+++ GSAP4E.js
@@ -1,13 +1,130 @@
-import * as g4eu from './GSAP4E_utils.js'
-
 gsap.registerPlugin(ScrollTrigger);
 
 /**
  * Available options settable from Elementor
  */
+// For gsap animation and scrolltrigger
 var gsap4e_attributes = ['start', 'end', 'x', 'y', 'duration', 'ease', 'scrub', 'toggleActions',
-                        'markers', 'no-mobile', 'no-tablet', 'scale', 'opacity', 'pin', 'pinSpacing', 'immediateRender'];
+                        'markers', 'scale', 'opacity', 'pin', 'pinSpacing', 'immediateRender',
+						'stagger', 'stagger-each', 'stagger-from', 'stagger-grid', 'stagger-ease',
+                        'stagger-amount', 'rotationX', 'rotationY', 'transforOrigin', 'delay'];
 
+                        // For other general purpose settings
+gsap4e_attributes.push('animateChild', 'overflowHidden', 'animationTo', 'no-mobile', 'no-tablet');
+
+// To define predefined animations
+gsap4e_attributes.push('anim1', 'elementScale1', 'fadeIn');
+
+class gsap4eParameter{
+    constructor(g4ees) {
+        this.param = {};
+        this.animationType = ('animationTo' in g4ees) ? 'to': 'from';
+
+        // Animation parameters
+        if('y' in g4ees) this.param.y = g4ees.y;
+        if('x' in g4ees) this.param.x = g4ees.x;
+        if('opacity' in g4ees) this.param.opacity = g4ees.opacity;
+        if('duration' in g4ees) this.param.duration = g4ees.duration;
+        if('ease' in g4ees) this.param.ease = g4ees.ease;
+        if('immediateRender' in g4ees) this.param.immediateRender = g4ees.immediateRender;
+        if('rotationX' in g4ees) this.param.rotationX = g4ees.rotationX;
+        if('rotationY' in g4ees) this.param.rotationY = g4ees.rotationY;
+        if('scale' in g4ees) this.param.scale = g4ees.scale;
+        if('transformOrigin' in g4ees) this.param.transformOrigin = g4ees.transformOrigin;
+        if('delay' in g4ees) this.param.delay = g4ees.delay;
+
+        this.param.stagger = {};
+        if('stagger' in g4ees) this.param.stagger.each = g4ees.stagger;
+        if('stagger-each' in g4ees) this.param.stagger.each = g4ees['stagger-each'];
+        if('stagger-from' in g4ees) this.param.stagger.from = g4ees['stagger-from'];
+        if('stagger-grid' in g4ees) this.param.stagger.grid = g4ees['stagger-grid'];
+        if('stagger-ease' in g4ees) this.param.stagger.ease = g4ees['stagger-ease'];
+        if('stagger-amount' in g4ees) this.param.stagger.amount = g4ees['stagger-amount'];
+        
+        // Scrolltrigger parameters
+        this.param.scrollTrigger = {};
+        if('start' in g4ees) this.param.scrollTrigger.start = g4ees.start;
+        if('end' in g4ees) this.param.scrollTrigger.end = g4ees.end;
+        if('scrub' in g4ees) this.param.scrollTrigger.scrub = g4ees.scrub;
+        if('pin' in g4ees) this.param.scrollTrigger.pin = true;
+        if('pinSpacing' in g4ees) this.param.scrollTrigger.pinSpacing = true;
+        if('toggleActions' in g4ees) this.param.scrollTrigger.toggleActions = g4ees.toggleActions;
+        if('markers' in g4ees) this.param.scrollTrigger.markers = true;
+    }
+}
+
+//************************************************************************************************//
+//                                                                                                //
+//                                        Helper functions                                        //
+//                                                                                                //
+//************************************************************************************************//              
+/**
+ * Check is the screen width is mobile (<= 768px)
+ * @returns true/false
+ */
+function isMobile(){
+    return window.innerWidth <= 768;
+}
+
+/**
+ * Chech is the screen width is mobile or tablet (<= 1024px)
+ * @returns true/false
+ */
+function isTabletOrMobile(){
+    return window.innerWidth <= 1024;
+}
+
+function get_gsap4e_attributes(element){
+    var settings = {};
+    gsap4e_attributes.forEach(attribute => {
+        if(element.hasAttribute('data-gsap4e-' + attribute)){
+            settings[attribute] = element.getAttribute('data-gsap4e-' + attribute);
+        }
+    });
+    return settings;
+}
+
+function disableTransformTransition(element) {
+    let currentTransition = window.getComputedStyle(element).transition;
+    let newTransition = currentTransition
+        .split(',')
+        .filter(transition => !transition.includes('transform'))
+        .join(',');
+    element.style.transition = newTransition;
+}
+
+function invertCssDistance(distance) {
+    const regex = /^(-?\d*\.?\d+)([a-z%]*)$/;
+    const match = distance.match(regex);
+    
+    if (!match) return 0;
+
+    const value = parseFloat(match[1]);
+    const unit = match[2];
+  
+    const invertedValue = -value;
+  
+    return invertedValue + unit;
+}
+
+let gsap4e_fadeIn = document.querySelectorAll('.gsap4e-fadeIn');
+gsap4e_fadeIn.forEach(element => {
+    let gsap4e_element_settings = get_gsap4e_attributes(element);
+    if(!('no-mobile' in gsap4e_element_settings && isMobile()) && !('no-tablet' in gsap4e_element_settings && isTabletOrMobile())){
+        gsap.from(element, {
+            opacity: 0,
+            duration: ('duration' in gsap4e_element_settings) ? gsap4e_element_settings['duration'] : '0.5',
+            ease: ('ease' in gsap4e_element_settings) ? gsap4e_element_settings['ease'] : 'power1.inOut',
+            immediateRender: ('immediateRender' in gsap4e_element_settings) ? gsap4e_element_settings.immediateRender : true,
+            scrollTrigger: {
+                trigger: element,
+                start: ('start' in gsap4e_element_settings) ? gsap4e_element_settings['start'] : 'top 80%',
+                toggleActions: ('toggleActions' in gsap4e_element_settings) ? gsap4e_element_settings['toggleActions'] : 'play none none reverse',
+                markers: ('markers' in gsap4e_element_settings) ? 'true' : false,
+            }
+        });
+    }
+});
 
 
 
@@ -25,10 +142,10 @@ gsap4e_imgAnim1.forEach(e => {
             let e_img = e_ic.firstElementChild;
             // Set custom styles
             e.style.overflow = "hidden";
-            g4eu.disableTransformTransition(e_ic);
+            disableTransformTransition(e_ic);
             e_ic.style.overflow = "hidden";
-            let gsap4e_element_settings = g4eu.get_gsap4e_attributes(e);
-            if(!('no-mobile' in gsap4e_element_settings && g4eu.isMobile()) && !('no-tablet' in gsap4e_element_settings && g4eu.isTabletOrMobile())){
+            let gsap4e_element_settings = get_gsap4e_attributes(e);
+            if(!('no-mobile' in gsap4e_element_settings && isMobile()) && !('no-tablet' in gsap4e_element_settings && isTabletOrMobile())){
                 // Animation
                 let e_tl = gsap.timeline({
                     immediateRender: ('immediateRender' in gsap4e_element_settings) ? gsap4e_element_settings.immediateRender : true,
@@ -44,9 +161,9 @@ gsap4e_imgAnim1.forEach(e => {
                     }
                 });
                 let yVal = ('y' in gsap4e_element_settings) ? gsap4e_element_settings.y : '0';
-                let yValI = g4eu.invertCssDistance(yVal);
+                let yValI = invertCssDistance(yVal);
                 let xVal = ('x' in gsap4e_element_settings) ? gsap4e_element_settings.x : '0';
-                let xValI = g4eu.invertCssDistance(xVal);
+                let xValI = invertCssDistance(xVal);
                 e_tl.from(e_ic, {
                     y: yVal,
                     x: xVal,
@@ -76,16 +193,17 @@ gsap4e_headingSlide.forEach(e => {
         let e_ic = e.firstElementChild; // Get inner container
         if(e_ic.hasChildNodes()){
             let e_heading = e_ic.firstElementChild;
-            let gsap4e_element_settings = g4eu.get_gsap4e_attributes(e);
-            if(!('no-mobile' in gsap4e_element_settings && g4eu.isMobile()) && !('no-tablet' in gsap4e_element_settings && g4eu.isTabletOrMobile())){
+            let gsap4e_element_settings = get_gsap4e_attributes(e);
+            if(!('no-mobile' in gsap4e_element_settings && isMobile()) && !('no-tablet' in gsap4e_element_settings && isTabletOrMobile())){
                 // Set custom styles
-                g4eu.disableTransformTransition(e_ic);
+                disableTransformTransition(e_ic);
                 e.style.overflow = 'hidden';
                 e_heading.style.display = 'block'; // Otherwise <span> does not work
                 // Animation
                 gsap.from(e_heading, {
                     y: ('y' in gsap4e_element_settings) ? gsap4e_element_settings.y : '0',
                     x: ('x' in gsap4e_element_settings) ? gsap4e_element_settings.x : '0',
+                    opacity: ('opacity' in gsap4e_element_settings) ? gsap4e_element_settings.opacity : '1',
                     duration: ('duration' in gsap4e_element_settings) ? gsap4e_element_settings.duration : '1',
                     ease: ('ease' in gsap4e_element_settings) ? gsap4e_element_settings.ease : 'power1.out',
                     immediateRender: ('immediateRender' in gsap4e_element_settings) ? gsap4e_element_settings.immediateRender : true,
@@ -115,10 +233,10 @@ gsap4e_textSlide.forEach(e => {
     if(e.hasChildNodes()){
         let e_ic = e.firstElementChild;
         if(e_ic.hasChildNodes()){
-            let gsap4e_element_settings = g4eu.get_gsap4e_attributes(e);
-            if(!('no-mobile' in gsap4e_element_settings && g4eu.isMobile()) && !('no-tablet' in gsap4e_element_settings && g4eu.isTabletOrMobile())){
+            let gsap4e_element_settings = get_gsap4e_attributes(e);
+            if(!('no-mobile' in gsap4e_element_settings && isMobile()) && !('no-tablet' in gsap4e_element_settings && isTabletOrMobile())){
                 // Set custom styles
-                g4eu.disableTransformTransition(e_ic);
+                disableTransformTransition(e_ic);
                 e.style.overflow = 'hidden';
                 // Animation
                 e_ic.childNodes.forEach(function(node) {
@@ -151,33 +269,97 @@ gsap4e_textSlide.forEach(e => {
 
 //************************************************************************************************//
 //                                                                                                //
-//                                         Element slide                                          //
+//                                         Element animation                                          //
 //                                                                                                //
 //************************************************************************************************//
 
-let gsap4e_elementSlide = document.querySelectorAll('.gsap4e-elementSlide');
+function setElemAnimPred(g4ep, g4ees){
+
+    if('elementScale1' in g4ees){
+        g4ep.param.ease = 'linear';
+        g4ep.param.scrollTrigger.start = 'center center';
+        g4ep.param.scrollTrigger.end = 'bottom top';
+        g4ep.param.scale = 1.5;
+        g4ep.param.scrollTrigger.toggleActions = 'play none none reverse';
+        g4ep.param.scrollTrigger.scrub = true;
+        g4ep.animationType = 'to';
+    }
+    if('fadeIn' in g4ees){
+        g4ep.param.opacity = 0;
+        g4ep.param.scrollTrigger.start = 'top 80%';
+        g4ep.param.duration = 1.5;
+        g4ep.param.scrollTrigger.toggleActions = 'play none none reset';
+    }
+}
+
+let gsap4e_elementSlide = document.querySelectorAll('.gsap4e-elemAnim');
 gsap4e_elementSlide.forEach(e => {
-    let gsap4e_element_settings = g4eu.get_gsap4e_attributes(e);
-    if(!('no-mobile' in gsap4e_element_settings && g4eu.isMobile()) && !('no-tablet' in gsap4e_element_settings && g4eu.isTabletOrMobile())){
+
+    let gsap4e_element_settings = get_gsap4e_attributes(e);
+    let e_toAnim = ('animateChild' in gsap4e_element_settings) ? e.firstElementChild : e;
+    if(!('no-mobile' in gsap4e_element_settings && isMobile()) && !('no-tablet' in gsap4e_element_settings && isTabletOrMobile())){
         // Set custom style
-        g4eu.disableTransformTransition(e);
+        disableTransformTransition(e);
+        e_toAnim.style.transition = 'none';
+        if('overflowHidden' in gsap4e_element_settings) e.style.overflow = 'hidden';
+
+
+        let g4ep = new gsap4eParameter(gsap4e_element_settings);
+        setElemAnimPred(g4ep, gsap4e_element_settings);
+        g4ep.param.scrollTrigger.trigger = e;
         // Animation
-        gsap.from(e, {
-            y: ('y' in gsap4e_element_settings) ? gsap4e_element_settings.y : '0',
-            x: ('x' in gsap4e_element_settings) ? gsap4e_element_settings.x : '0',
-            duration: ('duration' in gsap4e_element_settings) ? gsap4e_element_settings.duration : '0.5',
-            ease: ('ease' in gsap4e_element_settings) ? gsap4e_element_settings['ease'] : 'power2.out',
-            immediateRender: ('immediateRender' in gsap4e_element_settings) ? gsap4e_element_settings.immediateRender : true,
-            scrollTrigger: {
-                trigger: e,
-                start: ('start' in gsap4e_element_settings) ? gsap4e_element_settings.start : 'top 80%',
-                end: ('end' in gsap4e_element_settings) ? gsap4e_element_settings.end : 'bottom top',
-                scrub: ('scrub' in gsap4e_element_settings) ? true : false,
-                pin: ('pin' in gsap4e_element_settings) ? true : false,
-                pinSpacing: ('pinSpacing' in gsap4e_element_settings) ? true : false,
-                toggleActions: ('toggleActions' in gsap4e_element_settings) ? gsap4e_element_settings.toggleActions : 'play none none reverse',
-                markers: ('markers' in gsap4e_element_settings) ? true : false
-            }
-        });
+        if(g4ep.animationType === 'from')
+            gsap.from(e_toAnim, g4ep.param);
+        else{
+            gsap.to(e_toAnim, g4ep.param);
+            console.log('animatin tooo');
+        }
+
+    }
+});
+
+//************************************************************************************************//
+//                                                                                                //
+//                                        Text animation 1                                        //
+//                                                                                                //
+//************************************************************************************************//
+
+function setTextAnimPredefined(g4ep, g4ees){
+
+    // Default values
+    if(!('y' in g4ees)) g4ep.param.y = '100%';
+    if(!('start' in g4ees)) g4ep.param.scrollTrigger.start = 'top 80%';
+    if(!('end' in g4ees)) g4ep.param.scrollTrigger.end = 'bottom top';
+    if(!('toggleActions' in g4ees)) g4ep.param.scrollTrigger.toggleActions = 'play none none reset';
+    if(!('opacity' in g4ees)) g4ep.param.opacity = 0;
+
+    if('anim1' in g4ees){
+        g4ep.param.y = 20;
+        g4ep.param.transformOrigin = '0% 80%';
+        g4ep.param.ease = 'back';
+        g4ep.param.rotationX = 60;
+        g4ep.param.stagger.each = .1;
+    }
+}
+
+let gsap4e_textAnim1 = document.querySelectorAll('.gsap4e-textAnim');
+gsap4e_textAnim1.forEach(e => {
+    let gsap4e_element_settings = get_gsap4e_attributes(e);
+    if(!('no-mobile' in gsap4e_element_settings && isMobile()) && !('no-tablet' in gsap4e_element_settings && isTabletOrMobile())){
+        // Set custom style
+        disableTransformTransition(e);
+        if('overflowHidden' in gsap4e_element_settings) e.style.overflow = 'hidden';
+        
+        let text = e.querySelectorAll('h1, h2, h3, h4, h5, h6, span, p');
+        if(text){
+            let textSplit = new SplitType(text[0], { types: 'chars' });
+            let letters = e.querySelectorAll('.char');
+    
+            let g4ep = new gsap4eParameter(gsap4e_element_settings);
+            setTextAnimPredefined(g4ep, gsap4e_element_settings);
+            g4ep.param.scrollTrigger.trigger = e;
+            // Animation
+            gsap.from(letters, g4ep.param);
+        }
     }
 });
--- README.md
+++ README.md
@@ -1,45 +1,174 @@
 # GSAP4E
 GSAP For Elementor predefined basic animation
 
+This library does not cover all the possible animations, but if you need just something fast and easy to implement this can be good for you.
+
+**Note: Actually requires Elementor Pro to work**
+
 ## Usage
 
-## How it works
+Just three easy steps:
+1. Download the file and setup everithing as described below.
+2. Define the class of animation you want to apply in you elementor widget.
+3. In the attribute section of you widget define all the animation settings.
+
+## Setup
+
+### Requirements
+- Gsap
+- Gsap ScrollTrigger
+- Split Type
+
+### Include stuff
+
+Just include the GSAP4E.js file inside `functions.php` of your Wordpress child theme 
+
+```php
+function gsap4e_scripts() {	
+	wp_register_script('GSAP4E', get_stylesheet_directory_uri() . '/GSAP4E.js', array(), null, true);
+    wp_enqueue_script('GSAP4E');
+}
+add_action('wp_enqueue_scripts', 'gsap4e_scripts');
+```
+
+And include (in `functions.php`) also Gsap and Split Type libraries through CDNs. 
 
 ```php
 function enqueue_external_scripts() {
 	wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/gsap.min.js', array(), null);
 	wp_enqueue_script('gsap-scrollTrigger', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/ScrollTrigger.min.js', array(), null);
+	wp_enqueue_script('split-type', 'https://cdn.jsdelivr.net/npm/split-type@0.3.4/umd/index.min.js', array(), null);
 }
 add_action('wp_enqueue_scripts', 'enqueue_external_scripts');
 ```
 
+## Available parameteres (In general, read each animation description for more informations)
 
+### Gsap animation parameters
+| Name | Command | Gsap equivalent | Example(s) |
+|:---|:---|:---|:---|
+| Y | `data-gsap4e-y` | `y` | `data-gsap4e-y \| 500px` <br>  `data-gsap4e-y \| 100%` |
+| X | `data-gsap4e-x` | `x` | `data-gsap4e-x \| 500px` <br>  `data-gsap4e-x \| 100%` |
+| Opacity | `data-gsap4e-opacity` | `opacity` | `data-gsap4e-opacity \| 0.2` |
+| Duration | `data-gsap4e-duration` | `duration` | `data-gsap4e-duration \| 0.2` |
+| Ease | `data-gsap4e-ease` | `ease` | `data-gsap4e-ease \| linear` |
+| Immediate render | `data-gsap4e-immediateRender` | `immediateRender` | `data-gsap4e-immediateRender \| false` |
+| Rotation X | `data-gsap4e-rotationX` | `rotationX` | `data-gsap4e-rotationX \| 360` |
+| Rotation Y | `data-gsap4e-rotationY` | `rotationY` | `data-gsap4e-rotationY \| 360` |
+| Scale | `data-gsap4e-scale` | `scale` | `data-gsap4e-scale \| 2` |
+| Transform Origin | `data-gsap4e-transformOrigin` | `transformOrigin` | `data-gsap4e-transformOrigin \| 0% 0%` |
+| Dealy | `data-gsap4e-delay` | `delay` | `data-gsap4e-delay \| 0.5` |
+| Stagger | `data-gsap4e-stagger` | `stagger` | `data-gsap4e-stagger \| 0.1` |
+| Stagger each | `data-gsap4e-stagger-each` | `stagger.each` | `data-gsap4e-stagger-each \| 0.1` |
+| Stagger form | `data-gsap4e-stagger-from` | `stagger.from` | `data-gsap4e-stagger-from \| center` |
+| Stagger grid| `data-gsap4e-stagger-grid` | `stagger.grid` | `data-gsap4e-stagger-grid \| auto` |
+| Stagger ease | `data-gsap4e-stagger-ease` | `stagger.ease` | `data-gsap4e-stagger-ease \| power1.inOut` |
+| Stagger amount | `data-gsap4e-stagger-amount` | `stagger.amount` | `data-gsap4e-stagger-amount \| 1` |
+
+### Gsap scroltrigger parameters
+| Name | Command | Gsap equivalent | Example(s) |
+|:---|:---|:---|:---|
+| Start |  `data-gsap4e-start` | `start` | `data-gsap4e-start \| bottom center` <br> `data-gsap4e-start \| top += 500px` |
+| End | `data-gsap-end` | `end` | `data-gsap4e-end \| center center` <br> `data-gsap4e-end \| bottom 40%` |
+| Scrub | `data-gsap4e-scrub` | `scrub` | `data-gsap4e-scrub \| true` |
+| Pin | `data-gsap4e-pin` | `pin` | `data-gsap4e-pin \| true` |
+| Pin Spacing | `data-gsap4e-pinSpacing` | `pinSpacing`  | `data-gsap4e-pinSpacing \| true `|
+| Toggle actions | `data-gsap4e-toggleActions` | `toggleActions` | `data-gsap4e-toggleActions \| play none none reverse` |
+| Markers | `data-gsap4e-markers` | `markers` | `data-gsap4e-markers \| true` |
+
+### Other general purpose parameters
+| Name | Command | Description | Example(s) |
+|:---|:---|:---|:---|
+| Animate child | `data-gsap4e-animateChild` | If defined, the animation occours on the first child element | `data-gsap4e-animateChild` |
+| Overflow hidden | `data-gsap4e-overflowHidden` | If defined, the element overflow is set to hidden. This is useful in combination to 'Animate child' setting | `data-gsap4e-overflowHidden` |
+| Animation to | `data-gsap4e-animationTo` | the default animation is `gsap.from`. If this parameter is defined the animation changes to `gsap.to`, with the same parameters | `data-gsap4e-animationTo` |
+| Avoid animation on tablet | `data-gsap4e-no-tablet` | It checkes if the windows view size is <= 1024px. Not still working with resizing | `data-gsap4e-no-tablet` |
+| Avoid animation on mobile | `data-gsap4e-no-mobile` | It checkes if the windows view size is <= 768px. Not still working with resizing | `data-gsap4e-no-mobile` |
 
 ## Animations
 
 ### imgAnim1
 
-> Class | _.gsap4e-imgAnim1_
+> Class | `.gsap4e-textAnim`
 
-> Description |  
+> Description | Main text animation. It uses Split Type to divide text into letters, and apply fancier animations
 
-| Parameter name | Parameter command | Default value | Parameter type | Example(s) |
-|:---|:---|:---|:---|:---|
-| Start |  data-gsap4e-start | 'top 70%' | String | data-gsap4e-start \| bottom center <br> data-gsap4e-start \| top += 500px |
-| End | data-gsap-end | 'bottom top' | String | data-gsap4e-end \| center center <br> data-gsap4e-end \| bottom 40% |
-| Scrub | data-gsap4e-scrub | false | Bool | data-gsap4e-scrub \| true |
-| Pin | data-gsap4e-pin | false | Bool | data-gsap4e-pin \| true |
-| Pin Spacing | data-gsap4e-pinSpacing | false | Bool | data-gsap4e-pinSpacing \| true |
-| Toggle actions | data-gsap4e-toggleActions | 'play none none reverse' | String | |
-| Markers | data-gsap4e-markers | false | Bool | data-gsap4e-markers \| true |
-| Y | data-gsap4e-y | '0' | String | data-gsap4e-y \| 500px <br>  data-gsap4e-y \| 100% |
-| X | data-gsap4e-x | '0' | String | data-gsap4e-x \| 500px <br>  data-gsap4e-x \| 100% |
-| Ease | data-gsap4e-ease | 'power1.Out' | String | data-gsap4e-ease | linear |
-| Duration | data-gsap4e-duration | '1.5' | String | data-gsap4e-duration \| 0.2 |
-| Scale | data-gsap4e-scale | 1.3 | Int | data-gsap4e-scale \| 2 |
+> Note: The text must be in `<h1>`,`<h2>`,`<h3>`,`<h4>`,`<h5>`,`<hp>`,`<span>`,`<p>` tags, not others. 
 
-**Notes:**
-- To the element, and the inner container will be applied the style _overflow = hidden_
-- To the inner container will be disabled any transform transition
+#### Default values
+If not specified, the default parameters are the one defined in GSAP
 
+- `y: '100%'`
+- `start: 'top 80%'`
+- `end: 'bottom top'`
+- `toggleActions: 'play none none reset'`
+- `opacity: 0`
 
+#### Predefined animations
+
+##### Animation 1
+---
+
+Usage: `data-gsap4e-anim1`
+
+Default data:
+- `y: 20`
+- `transformOrigin: '0% 80%'`
+- `ease: 'back'`
+- `rotationX: 60`
+- `stagger.each: 0.1`
+---
+
+### elemAnim
+
+> Class | `.gsap4e-elemAnim`
+
+> Description | Main element animation. It just applies the defined animation on the element (or the child element). It's the most generaul-purpose animation
+
+#### Default values
+No default values here
+
+#### Predefined animations
+
+##### Element scale 1
+---
+
+Usage: `data-gsap4e-elementScale1`
+
+Description: It applies a `gsap.to` animation that scales the element
+
+Default data:
+- `ease: 'linear'`
+- `scrollTrigger.start: 'center center'`
+- `scrollTrigger.end: 'bottom top'`
+- `scale: 1.5`
+- `toggleActions: 'play none none reverse`
+- `scrub: true`
+---
+
+##### Fade in
+---
+
+Usage: `data-gsap4e-fadeIn`
+
+Description: It applies a `gsap.to` animation that scales the element
+
+Default data:
+- `ease: 'linear'`
+- `scrollTrigger.start: 'center center'`
+- `scrollTrigger.end: 'bottom top'`
+- `scale: 1.5`
+- `toggleActions: 'play none none reverse`
+- `scrub: true`
+---
+
+## Credits
+
+This project was made possible thanks to the incredible work of the GreenSock Animation Platform (GSAP) team. GSAP is a powerful, high-performance animation library used by developers worldwide to create stunning web animations. Its flexibility, performance, and ease of use have been invaluable in developing this library.
+
+GreenSock Animation Platform (GSAP):
+
+- Website: [Gsap website](https://gsap.com/)
+- License: [Gsap licening](https://gsap.com/licensing/)
+  
+Special thanks to the GreenSock team for providing such a robust tool that enables developers to bring their creative visions to life with ease.
